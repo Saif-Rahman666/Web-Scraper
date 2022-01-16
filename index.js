a5c02866f21da470146dfa546b9f35f15f15522d
Saif-Rahman666 <saf.saif@gmail.com>
2022-01-16 14:03:16 +0600

    updated

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const PORT = 8000
 const axios = require('axios')
 const cheerio = require('cheerio')
-const { response } = require('express')
+
 const express = require('express')
 
 const app = express()
 const url = 'https://www.pcworld.com/article/536545/slytricks.html'
 
 axios(url)
-
-    .then(response  => {
+    .then(response =>{
         const html = response.data
-        const $  = cheerio.load(html)
+        const $ = cheerio.load(html)
         const articles = []
 
-        $('.entry-title', html).each(function(){
-            const title= $(this).text()
+        $('.site-main', html).each(function() {
+            const title = $(this).text()
             const url = $(this).find('a').attr('href')
             articles.push({
                 title,
@@ -24,6 +23,9 @@ axios(url)
         })
         console.log(articles)
     }).catch(err => console.log(err))
+    
+
+    
 
   
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
